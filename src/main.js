73d1fcaa850e34624ce5a2630fdbcd116a693a99
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-18 13:23:08 +0700

    Use vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from "vue-router";
 
 import App from './App.vue';
+import TeamList from './components/teams/TeamsList.vue';
+import UserList from './components/users/UsersList.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/teams', component: TeamList },
+        { path: '/users', component: UserList },
+    ],
+    linkActiveClass: 'active'
+});
 
 const app = createApp(App)
+
+app.use(router);
 
 app.mount('#app');
