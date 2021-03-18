87bac7113b1702cedf124701500b32cfa9674c05
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-18 23:33:50 +0700

    Route Params

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from './App.vue';
 import TeamList from './components/teams/TeamsList.vue';
 import UserList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/teams', component: TeamList },
         { path: '/users', component: UserList },
+        { path: '/teams/:teamId', component: TeamMembers },
     ],
     linkActiveClass: 'active'
 });
